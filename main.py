0e17a6bb1baaa3b431c45543a557043cbca8f761
Jorge <jorgemail.affonso@gmail.com>
2023-01-23 13:47:58 -0300

    Day 37 - Add send_email function and introduce threads.

--- main.py
+++ main.py
@@ -1,10 +1,23 @@
+import glob
 import time
 import cv2
+import os
+from emailing import send_email
+from threading import Thread
 
 video = cv2.VideoCapture(0)
 first_frame = None
+status_list = []
+count = 1
+def clean_folder():
+    print("clean folder function started")
+    images = glob.glob("images/*.png")
+    for image in images:
+        os.remove(image)
+    print("clean folder function ended")
 
 while True:
+    status = 0
     time.sleep(1)
     check, frame = video.read()
     gray_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
@@ -17,13 +30,38 @@ while True:
     thresh_frame = cv2.threshold(delta_frame, 80, 255,cv2.THRESH_BINARY)[1]
     dil_frame = cv2.dilate(thresh_frame, None, iterations=2)
 
+    # Return a list of objets contournable
     contours, check = cv2.findContours(dil_frame, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
 
+
     for contour in contours:
-        if cv2.contourArea(contour) <10000:
+        if cv2.contourArea(contour) <5000:
             continue
         x, y, w, h = cv2.boundingRect(contour)
-        cv2.rectangle(frame, (x, y), (x+w, y+h), (0,255,0), 3)
+        rectangle = cv2.rectangle(frame, (x, y), (x+w, y+h), (0,255,0), 3)
+        if rectangle.any():
+            status = 1
+            cv2.imwrite(f"images/{count}.png", frame)
+            count = count + 1
+            all_images = glob.glob("images//*png")
+            index = int(len(all_images)/2)
+            image_with_obj = all_images[index]
+
+    status_list.append(status)
+    status_list = status_list[-2:]
+
+    if status_list[0] == 1 and status_list[1] == 0:
+        #send_email(image_with_obj)
+        email_thread = Thread(target=send_email, args=(image_with_obj, ))
+        email_thread.daemon = True
+
+        clean_thread = Thread(target=clean_folder)
+        clean_thread.daemon = True
+
+        email_thread.start()
+        print(f"email was send - {image_with_obj}")
+
+    print(status_list)
 
     cv2.imshow("Video", frame)
 
@@ -34,3 +72,6 @@ while True:
 
 video.release()
 
+clean_thread.start()
+
+time.sleep(2)
